2698d6830194334f2c5cbc7021c656f622f26fad
haripadhi08 <haripadhi08@gmail.com>
2023-11-02 01:47:24 +0530

    fixed format

--- index.js
+++ index.js
@@ -1,62 +1,62 @@
-class Planet{
-    constructor(game){
-        this.game = game;
-        this.x = this.game.width * 0.5;
-        this.y = this.game.height * 0.5;
-        this.radius = 80;
-        this.img = document.getElementById('planet');
-    }
+class Planet {
+  constructor(game) {
+    this.game = game;
+    this.x = this.game.width * 0.5;
+    this.y = this.game.height * 0.5;
+    this.radius = 80;
+    this.img = document.getElementById("planet");
+  }
 
-    draw(context){
-        context.drawImage(this.img,this.x - 100,this.y - 100); // 100 is to offset the position of the image to the center as the images are rect in nature
-        context.beginPath();
-        context.arc(this.x,this.y,this.radius,0,Math.PI * 2);
-        context.stroke();
-    }
+  draw(context) {
+    context.drawImage(this.img, this.x - 100, this.y - 100); // 100 is to offset the position of the image to the center as the images are rect in nature
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.stroke();
+  }
 }
 
 // Brain of the game codebase
-class Game{
-    constructor(canvas){
-        this.canvas = canvas;
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
-        this.planet = new Planet(this);
-        this.mouse = {
-            x: 0,
-            y: 0,
-        }
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.planet = new Planet(this);
+    this.mouse = {
+      x: 0,
+      y: 0,
+    };
 
-        window.addEventListener('mousemove',e => {
-            this.mouse.x = e.offsetX;
-            this.mouse.y = e.offsetY;
-        })
-    }
+    window.addEventListener("mousemove", (e) => {
+      this.mouse.x = e.offsetX;
+      this.mouse.y = e.offsetY;
+    });
+  }
 
-    render(context){
-        this.planet.draw(context);
-        context.beginPath();
-        context.moveTo(this.planet.x,this.planet.y);
-        context.lineTo(this.mouse.x,this.mouse.y);
-        context.stroke();
-    }
+  render(context) {
+    this.planet.draw(context);
+    context.beginPath();
+    context.moveTo(this.planet.x, this.planet.y);
+    context.lineTo(this.mouse.x, this.mouse.y);
+    context.stroke();
+  }
 }
 
-window.addEventListener('load',function () {
-    const canvas = this.document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 800;
-    canvas.height = 800;
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 2;
+window.addEventListener("load", function () {
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 800;
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 2;
 
-   const game = new Game(canvas);
+  const game = new Game(canvas);
 
-   function animate(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     game.render(ctx);
     requestAnimationFrame(animate);
-   }
+  }
 
-   this.requestAnimationFrame(animate);
-})
+  this.requestAnimationFrame(animate);
+});
