33b338586d9386a6c56d1cd63e822e2cac5fa1fe
haripadhi08 <haripadhi08@gmail.com>
2023-11-02 02:17:16 +0530

    complete the player setup

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ class Planet {
   }
 }
 
+class Player {
+  constructor(game) {
+    this.game = game;
+    this.x = this.game.width * 0.5;
+    this.y = this.game.height * 0.5;
+    this.radius = 40;
+    this.img = document.getElementById("player");
+    this.aim;
+  }
+
+  draw(context) {
+    context.drawImage(this.img, this.x - this.radius, this.y - this.radius);
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.stroke();
+  }
+
+  update() {
+    this.aim = this.game.calcAim(this.game.mouse, this.game.planet);
+    this.x = this.game.planet.x + 100 * this.aim[0];
+    this.y = this.game.planet.y + 100 * this.aim[1];
+  }
+}
+
 // Brain of the game codebase
 class Game {
   constructor(canvas) {
@@ -22,6 +46,7 @@ class Game {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.planet = new Planet(this);
+    this.player = new Player(this);
     this.mouse = {
       x: 0,
       y: 0,
@@ -35,10 +60,21 @@ class Game {
 
   render(context) {
     this.planet.draw(context);
+    this.player.draw(context);
+    this.player.update(context);
     context.beginPath();
     context.moveTo(this.planet.x, this.planet.y);
     context.lineTo(this.mouse.x, this.mouse.y);
     context.stroke();
+  }
+
+  calcAim(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    const distance = Math.hypot(dx, dy);
+    const aimX = dx / distance;
+    const aimY = dy / distance;
+    return [aimX, aimY, dx, dy];
   }
 }
 
